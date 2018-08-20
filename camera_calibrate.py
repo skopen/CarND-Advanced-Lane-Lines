3e3934c6d553966f0e9da4ab4d5c73413c552e83
skopen <sarang.kulkarni@gmail.com>
2018-08-19 21:36:33 -0700

    v2

--- camera_calibrate.py
+++ camera_calibrate.py
@@ -1,2 +1,59 @@
+import pickle
+import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
+# Read in the saved objpoints and imgpoints
+dist_pickle = pickle.load(open("wide_dist_pickle.p", "rb"))
+objpoints = dist_pickle["objpoints"]
+imgpoints = dist_pickle["imgpoints"]
+
+print (objpoints)
+print (imgpoints)
+
+# Read in an image
+img = cv2.imread('test_image.png')
+
+
+# TODO: Write a function that takes an image, object points, and image points
+# performs the camera calibration, image distortion correction and
+# returns the undistorted image
+def cal_undistort(img, objpoints, imgpoints):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Use cv2.calibrateCamera() and cv2.undistort()
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+
+    return undist
+
+# prepare object points
+nx = 8 #TODO: enter the number of inside corners in x
+ny = 6 #TODO: enter the number of inside corners in y
+
+# Make a list of calibration images
+#fname = 'calibration_test.png'
+#img = cv2.imread(fname)
+
+# Convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Find the chessboard corners
+ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+
+undistorted = cal_undistort(img, objpoints, imgpoints)
+
+
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=10)
+ax2.imshow(undistorted)
+ax2.set_title('Undistorted Image', fontsize=10)
+
+ax3.imshow(gray)
+
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+
+plt.show()
