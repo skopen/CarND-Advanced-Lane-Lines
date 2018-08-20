ec9b7e0577fb4d2a2bb30f9ae8f4d7c6e95a7e0e
skopen <sarang.kulkarni@gmail.com>
2018-08-20 10:42:17 -0700

    add polyfit

--- driver.py
+++ driver.py
@@ -1,8 +1,10 @@
 from camera_calibrate import *
 from camera_perspective import *
 from thresholded_binary import *
+from polyfit_init import *
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+
 
 calibrateCamera ("./camera_cal", 9, 6, False)
 configureTransformMatrix(False)
@@ -15,6 +17,7 @@ img = createThresholdedBinary(img)
 
 imgTrans = transform(img)
 
+out_img = fit_polynomial(imgTrans)
 
 
 
@@ -22,10 +25,10 @@ imgTrans = transform(img)
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
 
-ax1.imshow(img)
+ax1.imshow(imgTrans)
 ax1.set_title('Original Image', fontsize=40)
 
-ax2.imshow(imgTrans)
+ax2.imshow(out_img)
 ax2.set_title('Pipeline Result', fontsize=40)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 
