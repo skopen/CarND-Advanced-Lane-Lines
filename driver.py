d4e2e89320d66c1f51763d12d46e184424d13a5a
skopen <sarang.kulkarni@gmail.com>
2018-08-20 23:08:48 -0700

    added video processing

--- driver.py
+++ driver.py
@@ -5,6 +5,7 @@ from polyfit_init import *
 from polyfit_next import *
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from moviepy.editor import VideoFileClip
 
 
 calibrateCamera ("./camera_cal", 9, 6, False)
@@ -12,31 +13,34 @@ configureTransformMatrix(False)
 
 imgOrig = mpimg.imread("./test_images/straight_lines2.jpg")
 
-img = np.copy(imgOrig)
+def process_image(imgOrig):
+    img = np.copy(imgOrig)
 
-imgUndist = undistortImage(img)
+    imgUndist = undistortImage(img)
 
-img = createThresholdedBinary(imgUndist)
+    img = createThresholdedBinary(imgUndist)
 
-imgTrans = transform(img)
+    imgTrans = transform(img)
+
+    leftx, lefty, rightx, righty, out_img = find_lane_pixels(imgTrans)
+    left_fitx, right_fitx, ploty, left_fit, right_fit = fit_poly(imgTrans.shape, leftx, lefty, rightx, righty)
+
+    imgWithLaneMarked = search_around_poly(imgTrans, imgUndist, left_fit, right_fit)
+
+    return imgWithLaneMarked
 
 
-leftx, lefty, rightx, righty, out_img = find_lane_pixels(imgTrans)
-left_fitx, right_fitx, ploty, left_fit, right_fit = fit_poly(imgTrans.shape, leftx, lefty, rightx, righty)
+inputFile = "harder_challenge_video.mp4"
+outputFullFile = "output_images/output_" + inputFile
 
-imgWithLaneMarked = search_around_poly(imgTrans, imgUndist, left_fit, right_fit)
+## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
+## To do so add .subclip(start_second,end_second) to the end of the line below
+## Where start_second and end_second are integer values representing the start and end of the subclip
+## You may also uncomment the following line for a subclip of the first 5 seconds
+##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
+clip1 = VideoFileClip(inputFile).subclip(0, 5)
+white_clip = clip1.fl_image(process_image)
+white_clip.write_videofile(outputFullFile, audio=False)
 
-plt.imshow(imgWithLaneMarked)
 
-# Plot the result
-# f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-# f.tight_layout()
-#
-# ax1.imshow(imgTrans)
-# ax1.set_title('Original Image', fontsize=40)
-#
-# ax2.imshow(out_img)
-# ax2.set_title('Pipeline Result', fontsize=40)
-# plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-
-plt.show()
+#plt.show()
